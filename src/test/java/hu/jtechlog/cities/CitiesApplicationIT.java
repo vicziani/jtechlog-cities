a953df479ddae66847d34bfb39721385502ccc5b
vicziani <viczian.istvan@gmail.com>
2022-08-08 18:45:53 +0200

    Mock TemperatureGateway

--- src/test/java/hu/jtechlog/cities/CitiesApplicationIT.java
+++ src/test/java/hu/jtechlog/cities/CitiesApplicationIT.java
@@ -3,6 +3,7 @@ package hu.jtechlog.cities;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -10,6 +11,8 @@ import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @WebAppConfiguration
@@ -18,10 +21,14 @@ class CitiesApplicationIT {
 	@Autowired
 	WebApplicationContext context;
 
+	@MockBean
+	TemperatureGateway temperatureGateway;
+
 	@Test
 	void testGetCity() {
 		// RestAssured alapesetben float-ként olvassa be a JSON-ben lévő számot, így a CloseTo nem működik rá,
 		// ezért kell extract-ot hívni
+		when(temperatureGateway.getTemperature(any())).thenReturn("20");
 
 		double lat = given()
 			.webAppContextSetup(context)
