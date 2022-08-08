30ed1b9d8fd5d957f4d20548fbb4b94dffbef948
vicziani <viczian.istvan@gmail.com>
2022-08-08 21:05:54 +0200

    Spring Cloud Contract WireMock

--- src/test/java/hu/jtechlog/cities/TemperatureGatewayWireMockIT.java
+++ src/test/java/hu/jtechlog/cities/TemperatureGatewayWireMockIT.java
@@ -1,50 +1,20 @@
 package hu.jtechlog.cities;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.SocketUtils;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TemperatureGatewayWireMockIT {
+@SpringBootTest(properties = {"cities.idokep.url=http://localhost:${wiremock.server.port}/idokep.html#"})
+@AutoConfigureWireMock(port = 0, files = "*.html")
+class TemperatureGatewayWireMockIT {
 
-    static String host = "127.0.0.1";
-
-    static int port;
-
-    static WireMockServer wireMockServer;
-
-    @BeforeAll
-    static void startServer() {
-        port = SocketUtils.findAvailableTcpPort();
-        wireMockServer = new WireMockServer(wireMockConfig().port(port));
-        WireMock.configureFor(host, port);
-        wireMockServer.start();
-    }
-
-    @AfterAll
-    static void stopServer() {
-        wireMockServer.stop();
-    }
-
-    @BeforeEach
-    void resetServer() {
-        reset();
-    }
+    @Autowired
+    TemperatureGateway temperatureGateway;
 
     @Test
     void testGetTemperature() {
-        stubFor(get(urlEqualTo("/Budapest"))
-                .willReturn(aResponse().withBodyFile("idokep.html")));
-
-        var url = String.format("http://%s:%d/", host, port);
-        TemperatureGateway temperatureGateway = new TemperatureGateway(url);
         var temperature = temperatureGateway.getTemperature("Budapest");
         assertEquals("8°C", temperature);
     }
